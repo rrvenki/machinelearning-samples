ed6b8fab10e2638b05d5f919573602611f5f4052
Nukepayload2 <1939357182@qq.com>
2018-11-17 22:52:02 +0800

    Updated ImageClassification.Train

--- samples/visualbasic/getting-started/DeepLearning_TensorFlowEstimator/ImageClassification.Train/Model/ModelBuilder.vb
+++ samples/visualbasic/getting-started/DeepLearning_TensorFlowEstimator/ImageClassification.Train/Model/ModelBuilder.vb
@@ -1,13 +1,12 @@
-﻿Imports System.IO
+﻿Option Infer On
+Imports System.IO
 Imports ImageClassification.ImageData
 Imports Microsoft.ML.Runtime.Data
 Imports Microsoft.ML.Runtime.ImageAnalytics
 Imports Microsoft.ML.Transforms
-Imports Microsoft.ML.Runtime
 Imports Microsoft.ML
 Imports Microsoft.ML.Trainers
 Imports Microsoft.ML.Runtime.Api
-Imports Microsoft.ML.Transforms.Categorical
 Imports Microsoft.ML.Transforms.Conversions
 
 Namespace ImageClassification.Model
@@ -16,14 +15,14 @@ Namespace ImageClassification.Model
         Private ReadOnly imagesFolder As String
         Private ReadOnly inputModelLocation As String
         Private ReadOnly outputModelLocation As String
-        Private ReadOnly env As IHostEnvironment
+        Private ReadOnly mlContext As MLContext
 
-        Public Sub New(dataLocation As String, imagesFolder As String, inputModelLocation As String, outputModelLocation As String)
+        Public Sub New(ByVal dataLocation As String, ByVal imagesFolder As String, ByVal inputModelLocation As String, ByVal outputModelLocation As String)
             Me.dataLocation = dataLocation
             Me.imagesFolder = imagesFolder
             Me.inputModelLocation = inputModelLocation
             Me.outputModelLocation = outputModelLocation
-            env = New ConsoleEnvironment(seed:=1)
+            mlContext = New MLContext(seed:=1)
         End Sub
 
         Private Structure ImageNetSettings
@@ -43,18 +42,14 @@ Namespace ImageClassification.Model
             Console.WriteLine($"Training file: {dataLocation}")
             Console.WriteLine($"Default parameters: image size=({ImageNetSettings.imageWidth},{ImageNetSettings.imageHeight}), image mean: {ImageNetSettings.mean}")
 
-
-
-            Dim loader = New TextLoader(env, New TextLoader.Arguments With {
+            Dim loader = New TextLoader(mlContext, New TextLoader.Arguments With {
                 .Column = {
                     New TextLoader.Column("ImagePath", DataKind.Text, 0),
                     New TextLoader.Column("Label", DataKind.Text, 1)
                 }
             })
 
-
-
-            Dim pipeline = (New ValueToKeyMappingEstimator(env, "Label", "LabelTokey")).Append(New ImageLoadingEstimator(env, imagesFolder, ("ImagePath", "ImageReal"))).Append(New ImageResizingEstimator(env, "ImageReal", "ImageReal", ImageNetSettings.imageHeight, ImageNetSettings.imageWidth)).Append(New ImagePixelExtractingEstimator(env, {New ImagePixelExtractorTransform.ColumnInfo("ImageReal", "input", interleave:=ImageNetSettings.channelsLast, offset:=ImageNetSettings.mean)})).Append(New TensorFlowEstimator(env, featurizerModelLocation, {"input"}, {"softmax2_pre_activation"})).Append(New SdcaMultiClassTrainer(env, "softmax2_pre_activation", "LabelTokey")).Append(New KeyToValueEstimator(env, ("PredictedLabel", "PredictedLabelValue")))
+            Dim pipeline = mlContext.Transforms.Categorical.MapValueToKey("Label", "LabelTokey").Append(New ImageLoadingEstimator(mlContext, imagesFolder, ("ImagePath", "ImageReal"))).Append(New ImageResizingEstimator(mlContext, "ImageReal", "ImageReal", ImageNetSettings.imageHeight, ImageNetSettings.imageWidth)).Append(New ImagePixelExtractingEstimator(mlContext, {New ImagePixelExtractorTransform.ColumnInfo("ImageReal", "input", interleave:=ImageNetSettings.channelsLast, offset:=ImageNetSettings.mean)})).Append(New TensorFlowEstimator(mlContext, featurizerModelLocation, {"input"}, {"softmax2_pre_activation"})).Append(New SdcaMultiClassTrainer(mlContext, "softmax2_pre_activation", "LabelTokey")).Append(New KeyToValueEstimator(mlContext, ("PredictedLabel", "PredictedLabelValue")))
 
             ' Train the pipeline
             ConsoleWriteHeader("Training classification model")
@@ -65,11 +60,11 @@ Namespace ImageClassification.Model
             ' This is an optional step, but it's useful for debugging issues
             Dim trainData = model.Transform(data)
             Dim loadedModelOutputColumnNames = trainData.Schema.GetColumnNames()
-            Dim trainData2 = trainData.AsEnumerable(Of ImageNetPipeline)(env, False, True).ToList()
+            Dim trainData2 = trainData.AsEnumerable(Of ImageNetPipeline)(mlContext, False, True).ToList()
             trainData2.ForEach(Sub(pr) ConsoleWriteImagePrediction(pr.ImagePath, pr.PredictedLabelValue, pr.Score.Max()))
 
             ' Get some performance metric on the model using training data            
-            Dim sdcaContext = New MulticlassClassificationContext(env)
+            Dim sdcaContext = New MulticlassClassificationContext(mlContext)
             ConsoleWriteHeader("Classification metrics")
             Dim metrics = sdcaContext.Evaluate(trainData, label:="LabelTokey", predictedLabel:="PredictedLabel")
             Console.WriteLine($"LogLoss is: {metrics.LogLoss}")
@@ -79,7 +74,7 @@ Namespace ImageClassification.Model
             ConsoleWriteHeader("Save model to local file")
             DeleteAssets(outputModelLocation)
             Using f = New FileStream(outputModelLocation, FileMode.Create)
-                model.SaveTo(env, f)
+                model.SaveTo(mlContext, f)
             End Using
             Console.WriteLine($"Model saved: {outputModelLocation}")
         End Sub
