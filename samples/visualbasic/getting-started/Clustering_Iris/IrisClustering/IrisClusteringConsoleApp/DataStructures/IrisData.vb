97d075a2a89a3f29758dcf27e2aeb98910395af0
Nukepayload2 <1939357182@qq.com>
2018-12-08 14:16:46 +0800

    Converted Clustering_Iris

--- samples/visualbasic/getting-started/Clustering_Iris/IrisClustering/IrisClusteringConsoleApp/DataStructures/IrisData.vb
+++ samples/visualbasic/getting-started/Clustering_Iris/IrisClustering/IrisClusteringConsoleApp/DataStructures/IrisData.vb
@@ -5,20 +5,14 @@ Imports System.Text
 
 Namespace Clustering_Iris.DataStructures
 	Public Class IrisData
-		<Column("0")>
 		Public Label As Single
 
-		<Column("1")>
 		Public SepalLength As Single
 
-		<Column("2")>
 		Public SepalWidth As Single
 
-		<Column("3")>
 		Public PetalLength As Single
 
-		<Column("4")>
 		Public PetalWidth As Single
-
 	End Class
 End Namespace
--- samples/visualbasic/getting-started/Clustering_Iris/IrisClustering/IrisClusteringConsoleApp/Program.vb
+++ samples/visualbasic/getting-started/Clustering_Iris/IrisClustering/IrisClusteringConsoleApp/Program.vb
@@ -5,6 +5,7 @@ Imports Microsoft.ML
 Imports Common
 Imports Clustering_Iris.DataStructures
 Imports Microsoft.ML.Runtime.Data
+Imports Microsoft.ML.Core.Data
 
 Namespace Clustering_Iris
     Friend Module Program
@@ -20,60 +21,81 @@ Namespace Clustering_Iris
         Private BaseModelsPath As String = "../../../../MLModels"
         Private ModelPath As String = $"{BaseModelsPath}/IrisModel.zip"
 
-        Public Sub Main(args() As String)
+        Public Sub Main(ByVal args() As String)
             'Create the MLContext to share across components for deterministic results
             Dim mlContext As New MLContext(seed:=1) 'Seed set to any number so you have a deterministic environment
 
             ' STEP 1: Common data loading configuration
-            Dim textLoader = CreateTextLoader(mlContext)
-            Dim fullData = textLoader.Read(DataPath)
+            Dim textLoader As TextLoader = mlContext.Data.TextReader(New TextLoader.Arguments() With {
+                .Separator = vbTab,
+                .HasHeader = True,
+                .Column = {
+                    New TextLoader.Column("Label", DataKind.R4, 0),
+                    New TextLoader.Column("SepalLength", DataKind.R4, 1),
+                    New TextLoader.Column("SepalWidth", DataKind.R4, 2),
+                    New TextLoader.Column("PetalLength", DataKind.R4, 3),
+                    New TextLoader.Column("PetalWidth", DataKind.R4, 4)
+                }
+            })
 
+            Dim fullData As IDataView = textLoader.Read(DataPath)
+
+            'Split dataset in two parts: TrainingDataset (80%) and TestDataset (20%)
+            'INSTANT VB TODO TASK: VB has no equivalent to C# deconstruction declarations:
             With mlContext.Clustering.TrainTestSplit(fullData, testFraction:=0.2)
-                Dim trainingDataView = .trainSet, testingDataView = .testSet
+                Dim trainingDataView = .trainSet
+                Dim testingDataView = .testSet
 
                 'STEP 2: Process data transformations in pipeline
                 Dim dataProcessPipeline = mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
 
                 ' (Optional) Peek data in training DataView after applying the ProcessPipeline's transformations  
-                PeekDataViewInConsole(Of IrisData)(mlContext, trainingDataView, dataProcessPipeline, 10)
-                PeekVectorColumnDataInConsole(mlContext, "Features", trainingDataView, dataProcessPipeline, 10)
+                Common.ConsoleHelper.PeekDataViewInConsole(Of IrisData)(mlContext, trainingDataView, dataProcessPipeline, 10)
+                Common.ConsoleHelper.PeekVectorColumnDataInConsole(mlContext, "Features", trainingDataView, dataProcessPipeline, 10)
 
-                ' STEP 3: Create and train the model                
-                Dim modelBuilder = New ModelBuilder(Of IrisData, IrisPrediction)(mlContext, dataProcessPipeline)
+                ' STEP 3: Create and train the model     
                 Dim trainer = mlContext.Clustering.Trainers.KMeans(features:="Features", clustersCount:=3)
-                modelBuilder.AddTrainer(trainer)
-                Dim trainedModel = modelBuilder.Train(trainingDataView)
+                Dim trainingPipeline = dataProcessPipeline.Append(trainer)
+                Dim trainedModel = trainingPipeline.Fit(trainingDataView)
 
                 ' STEP4: Evaluate accuracy of the model
-                Dim metrics = modelBuilder.EvaluateClusteringModel(testingDataView)
-                PrintClusteringMetrics(trainer.ToString(), metrics)
+                Dim predictions As IDataView = trainedModel.Transform(testingDataView)
+                Dim metrics = mlContext.Clustering.Evaluate(predictions, score:="Score", features:="Features")
+
+                ConsoleHelper.PrintClusteringMetrics(trainer.ToString(), metrics)
 
                 ' STEP5: Save/persist the model as a .ZIP file
-                modelBuilder.SaveModelAsFile(ModelPath)
+                Using fs = New FileStream(ModelPath, FileMode.Create, FileAccess.Write, FileShare.Write)
+                    mlContext.Model.Save(trainedModel, fs)
+                End Using
+
+                Console.WriteLine("=============== End of training process ===============")
+
+                Console.WriteLine("=============== Predict a cluster for a single case (Single Iris data sample) ===============")
+
+                ' Test with one sample text 
+                Dim sampleIrisData = New IrisData() With {
+                    .SepalLength = 3.3F,
+                    .SepalWidth = 1.6F,
+                    .PetalLength = 0.2F,
+                    .PetalWidth = 5.1F
+                }
+
+                Dim model As ITransformer
+                Using stream = New FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read)
+                    model = mlContext.Model.Load(stream)
+                End Using
+
+                ' Create prediction engine related to the loaded trained model
+                Dim predFunction = trainedModel.MakePredictionFunction(Of IrisData, IrisPrediction)(mlContext)
+
+                'Score
+                Dim resultprediction = predFunction.Predict(sampleIrisData)
+                Console.WriteLine($"Cluster assigned for setosa flowers:" & resultprediction.SelectedClusterId)
             End With
-
-            Console.WriteLine("=============== End of training process ===============")
-
-            Console.WriteLine("=============== Predict a cluster for a single case (Single Iris data sample) ===============")
-
-            ' Test with one sample text 
-            Dim sampleIrisData = New IrisData() With {
-                .SepalLength = 3.3F,
-                .SepalWidth = 1.6F,
-                .PetalLength = 0.2F,
-                .PetalWidth = 5.1F
-            }
-
-            'Create the clusters: Create data files and plot a chart
-            Dim modelScorer = New ModelScorer(Of IrisData, IrisPrediction)(mlContext)
-            modelScorer.LoadModelFromZipFile(ModelPath)
-
-            Dim prediction = modelScorer.PredictSingle(sampleIrisData)
-
-            Console.WriteLine($"Cluster assigned for setosa flowers:" & prediction.SelectedClusterId)
-
             Console.WriteLine("=============== End of process, hit any key to finish ===============")
             Console.ReadKey()
         End Sub
     End Module
+
 End Namespace
